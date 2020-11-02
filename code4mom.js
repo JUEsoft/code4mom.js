3b7b507f5685a2b4a986fb57e2e50ff57a567f05
Timileyin <67488936+Timi-Leyin@users.noreply.github.com>
2020-11-02 22:18:46 +0000

    Removed unneccessary call which will lead to errors

--- code4mom.js
+++ code4mom.js
@@ -1,80 +1,73 @@
-
-    /* this is code for mom js
-    * simple library to work wioth dom easily 
+/* this is code for mom js
+    * simple library to work wioth dom easily
     * access your web cam easily code4mom.js
-    /created by Ejeh obiabo immanuel/
-    created in the year 2020 
+    /created by Ejeh Obiabo Immanuel/
+    created in the year 2020
     */
-   "use strict"
+"use strict"
 
-  console.warn('code4mom.js has been linked to your file successfully ')
-  console.info('code4mom.js easily let you manipulate dom eaily .....check the docs at https://code4mom-js.netlify.app')
-    const el= selector =>{
-        this.flow={
-            tag:document.querySelector(selector),
-            print:(val)=>flow.tag.innerHTML=`${val}`,
-            hide:()=>flow.tag.style.display='none',
-            block:()=>flow.tag.style.display='block',
-            on:(ev,caller)=>flow.tag.addEventListener(ev ,caller),
-            color:(col)=>flow.tag.style.color=`${col}`,
-            text:(letter)=>flow.tag.textContent=`${letter}`,
-            cls:( list )=>flow.tag.classList.add( list ),
-            remove:( list )=>flow.tag.classList.remove( list ),
-            webCam:()=>{
-                if(navigator.mediaDevices.getUserMedia){
-                    navigator.mediaDevices.getUserMedia({
-                        video:true
-                    }).then( stream =>{
-                        video=flow.tag
-                        video.srcObject=stream
-                    }).catch(err => alert('failed'))
-                } else {
-                    alert(' webcam not supported in your device')
-                }
-            },
-         
-            //get input value
-            value:(input)=>flow.tag.value ,
-            //prevent form action
-            prevent(){
-                flow.tag.onsubmit=e=>e.preventDefault()
-            }
-        }
-        return flow    
-   }
+console.info('Code4mom.js has been linked to your file successfully ')
+console.info('Code4mom.js easily let you manipulate dom eaily .....check the docs at https://code4mom-js.netlify.app')
+const el = selector => {
+  this.flow = {
+    tag: document.querySelector(selector),
+    print: (val)=>flow.tag.innerHTML = `${val}`,
+    hide: ()=>flow.tag.style.display = 'none',
+    block: ()=>flow.tag.style.display = 'block',
+    on: (ev, caller)=>flow.tag.addEventListener(ev, caller),
+    color: (col)=>flow.tag.style.color = `${col}`,
+    text: (letter)=>flow.tag.textContent = `${letter}`,
+    cls: (list)=>flow.tag.classList.add(list),
+    remove: (list)=>flow.tag.classList.remove(list),
+    webCam: ()=> {
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+          video: true
+        }).then(stream => {
+          video = flow.tag
+          video.srcObject = stream
+        }).catch(err => console.error('Could not connect with webcam'))
+      } else {
+       console.error(' webcam not supported in your device')
+      }
+    },
 
-//create element 
-const create= theEle =>{
-   const node={
-    elem:(text)=>{
-        let creat=document.createElement(theEle);
-    let append=document.createTextNode(text);
+    //get input value
+    value: (input)=>flow.tag.value,
+    //prevent form action
+    prevent() {
+      flow.tag.onsubmit = e=>e.preventDefault()
+    }
+  }
+  return flow
+}
+
+//create element
+const create = theEle => {
+  const node = {
+    elem: (text)=> {
+      let creat = document.createElement(theEle);
+      let append = document.createTextNode(text);
       creat.appendChild(append);
-      const body=document.querySelector('body');
+      const body = document.querySelector('body');
       body.appendChild(creat)
     }
-   }
-   return node
-    
-}
-el('form').prevent()
+  }
+  return node
 
-//viberate 
-const app=()=>{
-  const vib={
-    shake(duration){
-        navigator.vibrate([duration]);
+}
+
+
+//viberate
+const app = ()=> {
+  const vib = {
+    shake(duration) {
+      navigator.vibrate([duration]);
     }
 
-   }
-   return vib;
+  }
+  return vib;
 
 }
 
-console.log(el('input').value())
 
-
-  
-
- 
-  
